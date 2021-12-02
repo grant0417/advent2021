cabbc0c79e7bc3cfc3d8961c933181b9e3a12a22
grant0417 <grantgurvis@gmail.com>
2021-12-02 13:47:57 -0500

    Day 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -1,11 +1,53 @@
-// https://adventofcode.com/2021/day/1
+// https://adventofcode.com/2021/day/2
 
 pub fn part1() -> String {
-    String::new()
+    let input = include_str!("../../data/day2.txt");
+
+    let mut depth = 0;
+    let mut hor_distance = 0;
+
+    for line in input.lines() {
+        let mut data = line.split(' ');
+
+        let direction = data.next().unwrap();
+        let x = data.next().unwrap().parse::<i32>().unwrap();
+
+        match direction {
+            "forward" => hor_distance += x,
+            "up" => depth -= x,
+            "down" => depth += x,
+            _ => panic!("Unknown direction: {}", direction),
+        }        
+    }
+    
+    format!("{}", hor_distance * depth)
 }
 
 pub fn part2() -> String {
-    String::new()
+    let input = include_str!("../../data/day2.txt");
+
+    let mut aim = 0;
+    let mut hor_distance = 0;
+    let mut depth = 0;
+
+    for line in input.lines() {
+        let mut data = line.split(' ');
+
+        let direction = data.next().unwrap();
+        let x = data.next().unwrap().parse::<i32>().unwrap();
+
+        match direction {
+            "forward" => {
+                hor_distance += x;
+                depth += x * aim;
+            },
+            "up" => aim -= x,
+            "down" => aim += x,
+            _ => panic!("Unknown direction: {}", direction),
+        }        
+    }
+
+    format!("{}", hor_distance * depth)
 }
 
 #[cfg(test)]
@@ -14,11 +56,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(), "");
+        assert_eq!(part1(), "1815044");
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), "");
+        assert_eq!(part2(), "1739283308");
     }
 }
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,2 +1,5 @@
 pub mod day1;
 pub mod day2;
+
+#[allow(unused)]
+mod day_template;
